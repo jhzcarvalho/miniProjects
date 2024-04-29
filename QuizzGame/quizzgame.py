92d76fbd3cdd5e15ef4911e1fe154517ecec1d82
João Henrique Carvalho <jhzcarvalho@gmail.com>
2024-04-29 14:32:11 -0300

    Added QuizzGame
    Exercicio sobre a criação de um jogo de perguntas e respostas

---
+++ QuizzGame/quizzgame.py
@@ -0,0 +1,30 @@
+###
+# Crie um joguinho de pergutas e respostas que mostre uma pergunta qualquer 
+# para o usuário e forneça 4 alternativas ('A', 'B', 'C' e 'D') de resposta,
+# o programa deve receber a resposta do usuário e fazer as verificações 
+# necessárias, quando todas as porgundtas forem respondidas, o programa deve
+# mostrar a nota final do usuário.
+#
+# O programa deverá aceitar apenas as entradas que atendem as seguintes regras:
+# - A resposta deverá conter apenas uma unica letra;
+# - Essa letra deve ser correspondente à uma das alternativas ('A', 'B','C' e 'D');
+# - Não é necessário diferenciar entre maiúsculas e minúsculas, então as 
+#   entradas 'a', 'b', 'c' e 'd' são válidas
+#
+# Algumas etapas serão necessárias para realizar esse projeto, incluindo uma 
+# maneira de armazenar as perguntas do quizz, contendo um enunciado, lista de 
+# respostas e qual é a resposta correta, essas e outras decisões sobre como 
+# pedaços especícicos do programa devem funcionar ficam ao seu critério.
+# 
+# OBS: Utilize funções para ajudar a organizar o código, isso facilitará
+# o seu trabalho.
+#
+# Boa sorte!!
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
