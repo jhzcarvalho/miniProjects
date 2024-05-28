973bbdf6a4e5447131af983046819e47ba0c42d6
João Henrique Carvalho <jhzcarvalho@gmail.com>
2024-05-28 17:24:19 -0300

    update quizzgame.py

--- QuizzGame/quizzgame.py
+++ QuizzGame/quizzgame.py
@@ -21,9 +21,101 @@
 #
 # Boa sorte!!
 
+import time
+
+def jogar():
+    for i in range(5):
+        print("jogando...")
+        time.sleep(0.5)
+
+    print("Voltando ao menu!")
+
+
+def adiciona_questao():
+    print("Adicionando questão..")
+    time.sleep(3)
+
+
+def edita_questao():
+    print("Editando questão..")
+    time.sleep(3)
+
+
+def ativa_questao():
+    print("Ativando/Desativando questão..")
+    time.sleep(3)
+
+
+def remove_questao():
+    print("Removendo questão..")
+    time.sleep(3)
+
+
+def menu_questao():
+    
+    
+    opt = ""
+
+    while opt != "5":
+        print(
+            """
+            > Menu Questão <
+            """)
+        
+
+        print(
+            """
+            1. Adicionar questão 
+            2. Editar questão 
+            3. Ativar|Desativar qustão 
+            4. Remover questão 
+            5. Voltar ao menu principal
+            """
+        )
+    
+        opt = input('Selecione uma opção')
+
+        if opt == "1": adiciona_questao()
+
+        elif opt == "2": edita_questao()
+            
+        elif opt == "3": ativa_questao()
+
+        elif opt == "4": remove_questao()
+
+        elif opt == "5": print("Voltando ao Menu Principal..")
+
+        else: print("Selecione uma opção válida..")
+
 
 def main():
-    pass
+    """
+    Exemplo  de Menu
+    """
+
+    opt = ""
+
+    while opt != "3":
+        print("""
+            > Menu Principal <
+            """)
+        print(
+            """
+            1. Jogar
+            2. Editar Questões
+            3. Sair do Jogo
+            """)
+
+        opt = input('Selecione uma opção: ')
+
+        if opt == "1": jogar()
+
+        elif opt == "2": menu_questao()
+
+        elif opt == "3": print("Saindo do jogo..")
+
+        else: print("Selecione uma opção válida..")
+
 
 if __name__ == "__main__":
     main()
